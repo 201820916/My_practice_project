80e90b1dd4db922313de31d618b670e1bc002a83
Park Seong Hun <kkupsh2@gmail.com>
2025-12-05 18:15:15 +0900

    bouncyball Multy star
    bouncyball, more star

---
+++ Bouncyball_multystar/Assets/Script/Player.cs
@@ -0,0 +1,63 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float moveSpeed = 4;
+    public float jumpForce = 3;
+    public GameObject panel;
+
+    public List<GameObject> stars = new List<GameObject>();
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rb.linearVelocityX = h * moveSpeed;
+
+        if (transform.position.y < -5)
+        {
+            rb.linearVelocity = Vector2.zero;
+            transform.position = new Vector3(-7.48f, -2.42f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("enemy"))
+        {
+            transform.position = new Vector3(-7.48f, -2.42f);
+        }
+
+        else if ( collision.gameObject.CompareTag("finish"))
+        {
+            int index = Array.IndexOf(stars.ToArray(), collision.gameObject);
+            stars.Remove(stars[index]);
+            Destroy(collision.gameObject);
+
+            if(stars.Count == 0)
+            {
+                Time.timeScale = 0;
+                panel.SetActive(true);
+            }
+        }
+
+        else
+        {
+            Vector2 currentVelocity = rb.linearVelocity;
+            rb.linearVelocity = new Vector2(currentVelocity.x, jumpForce);
+        }
+    }
+}
